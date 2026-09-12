bdcd925534ba343b5e0c8bf504aff81e0fe73672
EvandroKabeloDev <115179511+EvandroKabeloDev@users.noreply.github.com>
2026-09-12 00:51:17 -0300

    Add desempenho.css to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { AuthProvider } from './lib/AuthContext'
 import './styles/global.css'
 import './styles/global.css'
 import './styles/etapa3-append.css'
+import './styles/desempenho.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
