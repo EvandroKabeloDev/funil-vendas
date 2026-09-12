6db15557787790ae150a7246d3f3226e082a7b72
EvandroKabeloDev <115179511+EvandroKabeloDev@users.noreply.github.com>
2026-09-12 00:44:42 -0300

    etapa 3: importa estilos do lançamento

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AuthProvider } from './lib/AuthContext'
 import './styles/global.css'
+import './styles/global.css'
+import './styles/etapa3-append.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
